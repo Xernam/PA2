be0e7974c479a42373b7418536a510127704390c
logan <loganm98@iastate.edu>
2019-04-24 15:58:15 -0500

    forgot to save

--- src/CommunicationsMonitor.java
+++ src/CommunicationsMonitor.java
@@ -1,24 +1,17 @@
-/**
- * @author Logan Williams
- */
-
-
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
-import CommunicationsMonitor.TripleComparator;
-
 public class CommunicationsMonitor {
 	
 	private List<ComputerTriple> tripleList;
-	private HashMap<Integer, List<ComputerNode>> nodeGraph; // need to create a graph of this somehow	
+	private HashMap<Integer, List<ComputerNode>> nodeGraph; // need to create a graph of this somehow
+	
 	
 	//Constructor with no parameters
 	public CommunicationsMonitor() {
-		tripleList = new ArrayList<ComputerTriple>();
+		tripleList = new LinkedList<ComputerTriple>();
 		nodeGraph = null;
 	}
 	
@@ -77,60 +70,28 @@ public class CommunicationsMonitor {
 				else
 					temp.getOutNeighbors().add(C1);
 			}
-				
 		}
 	}
 	
-	
-	
-	
-	
-	
-//	Determines whether computer c2 could be infected by time y if computer c1 was infected at time x. 
-//	If so, the method returns an ordered list of ComputerNode objects that represents the transmission sequence. 
-//	This sequence is a path in graph G. The first ComputerNodeobject on the path will correspond to c1. 
-//	Similarly, the last ComputerNodeobject on the path will correspond to c2. If c2 cannot be infected, return null.
-//  O(m)	
+	/**
+	 * Determines whether computer c2 could be infected by time y if computer c1 was infected at time x. 
+	 * @param c1 - computer 1
+	 * @param c2 - computer 2
+	 * @param x - time that c1 was infected
+	 * @param y - time to infect computer 2
+	 * @return list of nodes traversed to reach computer 2
+	 */
 	public List<ComputerNode> queryInfection(int c1, int c2, int x, int y){
-		
-		// What needs to be done here is to check the nodeGraph for a computer
-		// C1 that has time > x in the list attached to the c1 key, and check for
-		// a C2 that has time < y in the list attached to the c2 key. If both are
-		// true, then perform either DFS or BFS from C1 to C2, and return the list
-		// of nodes traversed. Choose either one you think will be easier (in terms
-		// of building the list, I think BFS would be better).
-		
-		boolean c1found = false;
-		boolean c2found = false; // you forgot to use the map to grab the list of computer nodes, just trying to grab
-								 // from the map of keys, which is invalid (this.nodeGraph.get(i).getID());
-		for(int i = 0; i < this.nodeGraph.size(); i++)
-		{
-			if(this.nodeGraph.getId() == c1 && this.nodeGraph.getId() == c2) {
-				if(this.nodeGraph.getTime() > x && this.nodeGraph.getTime() < y) {
-					ComputerNode c1Node = new ComputerNode(c1, x);
-					ComputerNode c2Node = new ComputerNode(c2, y);
-					return DFS(c1Node, c2Node, x, y);
-				}
-			}
-		}
-		
-		return null;
-	}
+		return BFS(c1, c2, x, y);
+	}	
 	
-	
-	
-	
-	
-//	Returns a HashMap that represents the mapping between an Integer and a list of ComputerNodeobjects.
-//	The Integer represents the ID of some computer Ci, while the list consists of pairs (Ci,t1),(Ci,t2),...,(Ci,tk), 
-//	represented by ComputerNode objects, that specify that Ci has communicated with other computers at times t1,t2,...,tk. 
-//	The list for each computer must be ordered by time; i.e.,t1<t2<���<tk
+	/**
+	 * Returns a HashMap that represents the mapping between an Integer and a list of ComputerNodeobjects.
+	 * @return nodeGraph - hashMap that represents the mapping between an Integer and a list
+	 */
 	public HashMap<Integer, List<ComputerNode>> getComputerMapping(){
 		return nodeGraph;
 	}
-	
-	
-	
 	
 //	Returns the list of ComputerNodeobjects associated with computer c by performing a lookup in the mapping.
 	public List<ComputerNode> getComputerMapping(int c){
@@ -139,37 +100,60 @@ public class CommunicationsMonitor {
 		return null;
 	}
 	
-	/**
-	 * performs a DFS search on a graph, using c1 as a starting point and searching for c2.
-	 * returns a list of nodes, signifying the path used to reach c2. If c2 is not found,
-	 * this method returns null; 
-	 * @param graph
-	 * @param c1
-	 * @param c2
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	private List<ComputerNode> DFS(ComputerNode c1, ComputerNode c2, int x, int y) {
-		
-		List<ComputerNode> path = new ArrayList<ComputerNode>();
-		path.add(c1);
-		path = DFSVisit(c1, c2, x, y, path);
-		if(!path.get(path.size() - 1 ).equals(c2))
-			return null;
-		return path;
-	}
-	
-	private List<ComputerNode> DFSVisit(ComputerNode c1, ComputerNode c2, int x, int y, List<ComputerNode> path){
-		c1.setColor(1);
-		for(int i = 0; i < c1.getOutNeighbors().size(); i++) {
-			ComputerNode temp = c1.getOutNeighbors().get(i);
+	private void uncolorList() {
+		for(ComputerTriple triple : tripleList) {
+			for(ComputerNode node : nodeGraph.get(triple.getC1().getID()))
+				node.setColor(0);
+			for(ComputerNode node : nodeGraph.get(triple.getC2().getID()))
+				node.setColor(0);
 		}
-		return path;
 	}
 	
-	class TripleComparator implements Comparator<ComputerTriple>{
+	private List<ComputerNode> BFS(int c1, int c2, int x, int y){
+		uncolorList();
+		LinkedList<ComputerNode> queue = new LinkedList<ComputerNode>();
+		LinkedList<ComputerNode> array = new LinkedList<ComputerNode>();
+		ComputerNode node = null;
+		ComputerNode endNode = null;
+		if(nodeGraph.get(c1) == null || nodeGraph.get(c2) == null)
+			return null;
+		for(int i = 0; i < nodeGraph.get(c1).size(); i++) {
+			if(nodeGraph.get(c1).get(i).getTimestamp() >= x) {
+				node = nodeGraph.get(c1).get(i);
+				break;
+			}
+		}
+		if(node == null)
+			return null;
+		node.setColor(1);
+		queue.addFirst(node);
+		while(!queue.isEmpty()) {
+			ComputerNode temp = queue.poll();
+			if(temp.getID() == c2 && temp.getTimestamp() <= y) {
+				endNode = temp;
+				break;
+			}
+			for(ComputerNode neighbor : temp.getOutNeighbors()) {
+				if(neighbor.getColor() == 0) {
+					queue.add(neighbor);
+					neighbor.setColor(1);
+					neighbor.setPred(temp);
+				}
+			}
+		}
+		if(endNode != null) {
+			ComputerNode temp = endNode.getPred();
+			array.addFirst(endNode);
+			while(temp != null) {
+				array.addFirst(temp);
+				temp = temp.getPred();
+			}
+			return array;
+		}
+		return null;
+	}
 
+	class TripleComparator implements Comparator<ComputerTriple>{
 		@Override
 		public int compare(ComputerTriple c1, ComputerTriple c2) {
 			if(c1.getTimestamp() < c2.getTimestamp())
@@ -179,7 +163,5 @@ public class CommunicationsMonitor {
 			else
 				return 0;
 		}
-		
 	}
-		
 }
